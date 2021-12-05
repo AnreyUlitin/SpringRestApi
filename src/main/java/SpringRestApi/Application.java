49eff5036a924d12eacd2f381648760bbfdbc31d
Andrey <anulitin@rambler.ru>
2021-12-05 21:14:31 +0300

    SpringRestApi

--- src/main/java/SpringRestApi/Application.java
+++ src/main/java/SpringRestApi/Application.java
@@ -1,11 +1,9 @@
 package SpringRestApi;
 
 
-import java.io.ObjectInputFilter;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 @SpringBootApplication
 public class Application {
